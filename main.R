304584e7c8f1327650cf0f0e4283c1cdfc09390d
Florian Jacquetin <florian.jacquetin@gmail.com>
2025-11-20 12:10:09 +0100

    Crée la jointure et enregistre une base de données macros zone euro

--- main.R
+++ main.R
@@ -84,8 +84,25 @@ prix <- get_data("ICP.M.U2.Y.XEFUN0.3.INX") %>%
 
 pi <- prix %>%
   mutate(pi=100*(prix)-lag(prix)) %>%
-  select(-prix) %>%
-  drop_na()
+  select(-prix)
+  # drop_na()
+masse_monetaire <- get_data("BSI.Q.U2.N.V.M30.X.1.U2.2300.Z01.E") %>%
+  select(date=obstime,m3=obsvalue) %>%
+  mutate(m3 = log(m3),
+         date=gsub("-","",date)) %>%
+  arrange(date) %>%
+  mutate(m3 = 100*(m3 - lag(m3)))
+  # drop_na()
+
+base <- get_data("ILM.M.U2.C.LT00001.Z5.EUR") %>%
+  select(date=obstime,base=obsvalue) %>%
+  mutate(date=as.yearmon(date, "%Y-%m"),
+         year = year(date),
+         quarter = quarter(date),
+         date = paste0(year, "Q",quarter)) %>%
+  group_by(date) %>%
+  summarise(base=mean(base,na.rm=TRUE)) %>%
+  ungroup()
 
 asset <- get_data("BSI.M.U2.N.A.T00.A.1.Z5.0000.Z01.E") %>%
   select(date=obstime,asset=obsvalue) %>%
@@ -98,3 +115,39 @@ asset <- get_data("BSI.M.U2.N.A.T00.A.1.Z5.0000.Z01.E") %>%
   ungroup() %>%
   mutate(asset = log(asset)) %>%
   arrange(date)
+
+taux <- get_data("FM.D.U2.EUR.4F.KR.MRR_FR.LEV") %>%
+  select(date=obstime,value=obsvalue) %>%
+  mutate(date=as.Date(date)) %>%
+  complete(date = seq.Date(min(date), max(date), by = "day")) %>%
+  mutate(value = na.locf(value)) %>%
+  mutate(
+    year = year(date),
+    quarter = quarter(date),
+    date = paste0(year, "Q",quarter)
+  ) %>%
+  group_by(date) %>%
+  summarise(taux = mean(value, na.rm = TRUE)) %>%
+  ungroup() %>%
+  arrange(date)
+
+change <- get_data("EXR.Q.USD.EUR.SP00.A") %>%
+  select(date=obstime,change=obsvalue) %>%
+  mutate(change = log(change),
+         date = gsub("-", "", date)) %>%
+  arrange(date) %>%
+  filter(!is.na(change))
+
+df = pib %>%
+  full_join(prix, by = "date") %>%
+  full_join(export, by = "date") %>%
+  full_join(taux, by = "date") %>%
+  full_join(conso, by = "date") %>%
+  full_join(cost, by = "date") %>%
+  full_join(fbcf, by = "date") %>%
+  full_join(stock, by = "date") %>%
+  full_join(change, by = "date") %>%
+  full_join(masse_monetaire, by = "date") %>%
+  arrange(date)
+
+write.xlsx(x=df,file="sorties/data.xlscx")
